5a90894dc8a63fa25412b695204453ffcfd4ff6a
elphie-pro <onazjr2006@gmail.com>
2025-10-11 22:08:25 +0100

    nav

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,14 @@
 "use client"
+// import Image from "next/image";
 
 export default function Page() {
     return (
-        <div>
-            <h1 className="text-white text-4xl font-black"> UTO</h1>
-            <p>Im testing the font</p>
+      <div className="w-screen block max-w-7xl mx-auto px-4 md:pb-2 ">
+        <div className="h-[10rem] bg-cover bg-center" style={{ backgroundImage: "url('/screenshot-2025-10-11-1.svg')" }}>
+            <h1 className="text-white bg text-4xl font-black font-fruktur">ụtọ</h1>
+        
+        </div>
+            <p className="font-fustat">Im testing the font</p>
         </div>
     );
 }
