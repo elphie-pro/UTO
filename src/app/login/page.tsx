392c77df09d304ff290d38d4dee075ef630b65ed
elphie-pro <onazjr2006@gmail.com>
2025-10-19 10:04:25 +0100

    signup

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,15 +4,15 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <div className="bg-[#f4f4f4] w-full block max-w-8xl mx-auto md:pb-14 px-12 pt-14">
+        <div className="bg-[#f4f4f4] w-full block max-w-8xl mx-auto md:pb-14 px-4 md:px-12 py-4 md:pt-14">
             <div className="flex justify-center items-center">
-                <div>
+                <div className="hidden md:block">
                     <Image src='/image 6.svg' alt="" width={10} height={10} className="w-full shadow-xl" />
                 </div>
                 <div className="w-[40rem] h-[52rem] bg-[#fff] p-10 shadow-xl">
                     <div className="flex flex-col gap-5">
                         <div className="flex justify-end">
-                             <h1 className="text-[#c4a77d] text-[8rem] mt-[-3rem] font-black font-fruktur">ụtọ</h1>
+                             <Link href='/'><h1 className="text-[#c4a77d] text-[6.5rem] mt-[-3rem] font-black font-fruktur">ụtọ</h1></Link>
                         </div>
                         <div className="">
                             <h1 className="text-4xl font-semibold">Welcome Back</h1>
@@ -27,12 +27,12 @@ export default function Page() {
                                 <label htmlFor="">Password</label>
                                 <input type="password" placeholder="**************" className="w-full h-[3rem] outline-none rounded-md p-2 bg-[#e8dfc9] opcaity-5"/>
                         </div>
-                        <div className="flex justify-between font-fustat">
+                        <div className="flex justify-between font-fustat text-sm md:text-lg">
                             <div className="flex gap-3">
                                  <input type="radio" name="" id="" className="w-[1rem] outline-none accent-[#e8dfc9]"/>
-                                 <p className="text-lg">Remember me</p>
+                                 <p className="">Remember me</p>
                             </div>
-                            <Link href='/' className="text-[#c4a77d] text-lg underline">Forgot Password</Link>
+                            <Link href='/' className="text-[#c4a77d] underline">Forgot Password</Link>
                         </div>
                         <div className="font-fustat">
                             <button className="w-full h-[3rem] bg-[#e8dfc9] text-[#c4a77d] text-xl rounded-md">Sign In</button>
--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -4,19 +4,19 @@ import Link from "next/link";
 
 export default function Page() {
     return (
-        <div className="bg-[#f4f4f4] w-full block max-w-8xl mx-auto md:pb-14 px-12 pt-14">
+        <div className="bg-[#f4f4f4] w-full block max-w-8xl mx-auto md:pb-14 px-4 md:px-12 py-4 md:pt-14">
             <div className="flex justify-center items-center">
-                <div>
+                <div className="hidden md:block">
                     <Image src='/image 6.svg' alt="" width={10} height={10} className="w-full shadow-xl" />
                 </div>
                 <div className="w-[40rem] h-[52rem] bg-[#fff] p-10 shadow-xl">
                     <div className="flex flex-col gap-3">
                         <div className="flex justify-end">
-                             <h1 className="text-[#c4a77d] text-[6.5rem] mt-[-3rem] font-black font-fruktur">ụtọ</h1>
+                             <Link href='/'><h1 className="text-[#c4a77d] text-[6.5rem] mt-[-3rem] font-black font-fruktur">ụtọ</h1></Link>
                         </div>
                         <div className="">
                             <h1 className="text-4xl font-semibold">Register</h1>
-                            <p className="font-fustat font-medium">Welcome to our website, please enter your details;</p>
+                            <p className="font-fustat font-medium text-sm md:text-[1rem]">Welcome to our website, <br className="md:hidden block"/>please enter your details;</p>
                             
                         </div>
                         <div className="flex flex-col gap-2 font-fustat">
@@ -36,9 +36,9 @@ export default function Page() {
                                 <input type="password" placeholder="**************" className="w-full h-[3rem] outline-none rounded-md p-2 bg-[#e8dfc9] opcaity-5"/>
                         </div>
                         <div className="flex justify-between font-fustat">
-                            <div className="flex gap-1">
+                            <div className="flex gap-1 mt-[-1rem]">
                                  <input type="radio" name="" id="" className="w-[1.5rem] outline-none accent-[#e8dfc9]"/>
-                                 <p className="text-md text-center mt-[1.5rem]">By creating an account, you agree to the Terms of Use and have read our Privacy Policy</p>
+                                 <p className="text-sm md:text-lg text-center mt-[1.5rem]">By creating an account, you agree to the Terms of Use and have read our Privacy Policy</p>
                             </div>
                         </div>
                         <div className="font-fustat">
