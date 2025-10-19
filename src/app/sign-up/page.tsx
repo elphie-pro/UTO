d862378084659976f004ca4dbcd2552a7b83cc66
elphie-pro <onazjr2006@gmail.com>
2025-10-19 09:40:07 +0100

    login

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -15,42 +15,33 @@ export default function Page() {
                              <h1 className="text-[#c4a77d] text-[8rem] mt-[-3rem] font-black font-fruktur">ụtọ</h1>
                         </div>
                         <div className="">
-                            <h1 className="text-4xl font-semibold">Welcome Back</h1>
-                            <p className="font-fustat font-medium">Please enter your details.</p>
+                            <h1 className="text-4xl font-semibold">Register</h1>
+                            <p className="font-fustat font-medium">Welcome to our website, please enter your details;</p>
                             
                         </div>
                         <div className="flex flex-col gap-2 font-fustat">
-                                <label htmlFor="">Email Address</label>
+                                <label htmlFor="">Name</label>
                                 <input type="text" placeholder="example@gmail.com" className="w-full h-[3rem] outline-none rounded-md p-2 bg-[#e8dfc9] opcaity-5"/>
+                        </div>
+                        <div className="flex flex-col gap-2 font-fustat">
+                                <label htmlFor="">Email Address</label>
+                                <input type="email" placeholder="example@gmail.com" className="w-full h-[3rem] outline-none rounded-md p-2 bg-[#e8dfc9] opcaity-5"/>
                         </div>
                         <div className="flex flex-col gap-2 font-fustat">
                                 <label htmlFor="">Password</label>
                                 <input type="password" placeholder="**************" className="w-full h-[3rem] outline-none rounded-md p-2 bg-[#e8dfc9] opcaity-5"/>
                         </div>
                         <div className="flex justify-between font-fustat">
-                            <div className="flex gap-3">
-                                 <input type="radio" name="" id="" className="w-[1rem] outline-none accent-[#e8dfc9]"/>
-                                 <p className="text-lg">Remember me</p>
+                            <div className="flex gap-1">
+                                 <input type="radio" name="" id="" className="w-[1.5rem] outline-none accent-[#e8dfc9]"/>
+                                 <p className="text-md text-center mt-[1.5rem]">By creating an account, you agree to the Terms of Use and have read our Privacy Policy</p>
                             </div>
-                            <Link href='/' className="text-[#c4a77d] text-lg underline">Forgot Password</Link>
                         </div>
                         <div className="font-fustat">
-                            <button className="w-full h-[3rem] bg-[#e8dfc9] text-[#c4a77d] text-xl rounded-md">Sign In</button>
+                            <button className="w-full h-[3rem] bg-[#e8dfc9] text-[#c4a77d] text-xl rounded-md">Sign Up</button>
                         </div>
                         <div className="flex flex-col gap-4 items-center justify-center font-fustat">
                             <h1 className="text-lg">Don’t Have an Account? <Link href='/sign-up' className="text-[#bf9d78]">Sign Up</Link></h1>
-                            <p className="text-5xl text-[#c4a77d]">OR</p>
-                            <div className="flex gap-9">
-                                <div>
-                                    <Link href='/'><Image src='/flat-color-icons_google.svg' alt="" width={30} height={30} className="w-full bg-[#f2ede1] p-3 rounded-full"/></Link>
-                                </div>
-                                <div>
-                                    <Link href='/'><Image src='/basil_apple-solid.svg' alt="" width={30} height={30} className="w-full bg-[#f2ede1] p-3 rounded-full"/></Link>
-                                </div>
-                                <div>
-                                    <Link href='/'><Image src='/logos_facebook.svg' alt="" width={30} height={30} className="w-full bg-[#f2ede1] p-3 rounded-full"/></Link>
-                                </div>
-                            </div>
                         </div>
                     </div> 
                 </div>
